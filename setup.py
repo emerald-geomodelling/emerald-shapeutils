3a5083220e3e588102c929896e032af3d97719f6
Duke-of-Lizard <cch@emeraldgeo.no>
2024-03-04 15:58:43 +0100

    version bump

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ import distutils.command.sdist
 
 setuptools.setup(
     name='emerald-shapeutils',
-    version='0.0.2',
+    version='0.0.3',
     description='Utils for handling shapes and rasters',
     long_description='Utils for handling shapes and rasters',
     long_description_content_type="text/markdown",
     author='Craig William Christensen, Egil Moeller and others ',
-    author_email='cch@emeraldgeo.no, em@emeraldgeo.no',
+    author_email='cch@emrld.no, em@emrld.no',
     url='https://github.com/emerald-geomodelling/emerald-shapeutils',
     packages=setuptools.find_packages(),
     include_package_data=True,
